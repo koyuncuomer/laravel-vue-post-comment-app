cd0e5e575a99421225dc17af7724fdbccaa472b9
Ömer Koyuncu <omerkoyuncutr@gmail.com>
2024-11-15 13:57:52 +0300

    frontend: init

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink class="nav-link" :to="{ name: 'home' }">Anasayfa</RouterLink>
+
+    </nav>
+
+  </header>
+
+  <RouterView />
+</template>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    <h1 class="title">Son Postlar</h1>
+  </main>
+</template>
