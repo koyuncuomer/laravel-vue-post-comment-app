9337cfadb0d3c2aab96c7d95971e32c75f7b63b5
Ömer Koyuncu <omerkoyuncutr@gmail.com>
2024-11-24 13:34:09 +0300

    backend: user admin yetkisi eklendi, comment update ve delete yetki ile düzenlendi

--- backend/app/Http/Controllers/CommentController.php
+++ backend/app/Http/Controllers/CommentController.php
@@ -5,9 +5,19 @@ namespace App\Http\Controllers;
 use App\Models\Comment;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Routing\Controllers\HasMiddleware;
+use Illuminate\Routing\Controllers\Middleware;
 
-class CommentController extends Controller
+class CommentController extends Controller implements HasMiddleware
 {
+    public static function middleware()
+    {
+        return [
+            new Middleware('auth:sanctum', except: ['index', 'store', 'show'])
+        ];
+    }
+
     /**
      * Display comments for a specific post.
      */
@@ -45,16 +55,18 @@ class CommentController extends Controller
      */
     public function update(Request $request, Comment $comment)
     {
-        // $fields = $request->validate([
-        //     'name' => 'required|max:255',
-        //     'email' => 'required|email|max:255',
-        //     'body' => 'required',
-        // ]);
+        Gate::authorize('modify', $comment);
 
-        // $comment->update($fields);
+        $fields = $request->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|email|max:255',
+            'body' => 'required',
+        ]);
 
-        // return $comment;
-        return ['message' => 'Yorum düzenlemek desteklenmemektedir.'];
+        $comment->update($fields);
+
+        return $comment;
+        //return ['message' => 'Yorum düzenlemek desteklenmemektedir.'];
 
     }
 
@@ -63,10 +75,12 @@ class CommentController extends Controller
      */
     public function destroy(Comment $comment)
     {
-        // $comment->delete();
+        Gate::authorize('modify', $comment);
 
-        // return ['message' => 'Yorum başarıyla silindi!'];
-        return ['message' => 'Yorum silmek desteklenmemektedir.'];
+        $comment->delete();
+
+        return ['message' => 'Yorum başarıyla silindi!'];
+        //return ['message' => 'Yorum silmek desteklenmemektedir.'];
 
     }
 }
